b6f807bc88c7d936e26e3d724b314a9021429df4
benjaminleroy <benjaminleroyusa@gmail.com>
2021-10-31 19:25:17 -0400

    updates to simulation conformal function to do both sbr approaches in 1

--- package/orbconformal/src/orbconformal/conformal_prediction.py
+++ package/orbconformal/src/orbconformal/conformal_prediction.py
@@ -12,7 +12,6 @@ from .psuedo_density import psuedo_density_multidim_func
 
 def simulation_based_conformal(true_z,
                                simulated_z,
-                               _per_time=False,
                                _sigma_string = "42.5%",
                                _small_ball_radius_string = "80%",
                                _maxT = 500,
@@ -31,11 +30,6 @@ def simulation_based_conformal(true_z,
         numpy array (n,m) of true function
     simulated_z : list
         list of numpy matrices (n,m)
-    _per_time: boolean
-        logical if we should calculate the small-ball-radius per time (the
-        "n" dimension of simulated_z). This will also effect the containment /
-        cs values - naturally. If False, the calculation will be
-        done across all time.
     _sigma_string : string
         string of percentage for the sigma-quantile to define the sigma
         relative to the pairwise distances between simulations
@@ -56,13 +50,13 @@ def simulation_based_conformal(true_z,
     verbose : boolean
         logic to be verbose about the progress of the function (TODO: needed?)
     """
-
     # convert percentages to proportions ----------
     _sigma_prop = check_character_percent(_sigma_string)
     _sbr_prop = check_character_percent(_small_ball_radius_string)
 
     # convert z_list to z_array -------------
     simulated_Z_array = np.array(simulated_z)
+    B = simulated_Z_array.shape[0]
 
     # calculate sigma_val ------------------
     dmat = l2_dist_matrix(simulated_z)
@@ -73,17 +67,17 @@ def simulation_based_conformal(true_z,
     pd_vec = psuedo_density_multidim_func(X_array=simulated_Z_array,
                                           sigma_string=_sigma_string)
 
-    # small-ball-radius value --------------
-    sbr_vec = calc_small_ball_rad_multidim_func(simulated_Z_array,
-                                                pd_vec,
-                                                per_time=_per_time)
-    if not _per_time:
-        sbr_val = sbr_vec[int(np.ceil(sbr_vec.shape[0]*_sbr_prop)-1)]
-    else:
-        sbr_val = sbr_vec[int(np.ceil(sbr_vec.shape[0]*_sbr_prop)-1),:]
+
+    # sbr_vec = calc_small_ball_rad_multidim_func(simulated_Z_array,
+    #                                             pd_vec,
+    #                                             per_time=_per_time)
+    # if not _per_time:
+    #     sbr_val = sbr_vec[int(np.ceil(sbr_vec.shape[0]*_sbr_prop)-1)]
+    # else:
+    #     sbr_val = sbr_vec[int(np.ceil(sbr_vec.shape[0]*_sbr_prop)-1),:]
+
+
     # mode clustering ---------------
-    #     if verbose:
-    #         print("mode clustering:")
 
     num_groups, group_df = mode_clustering(simulated_Z_array,
                                             sigma = sigma_val,
@@ -93,11 +87,34 @@ def simulation_based_conformal(true_z,
                                             parallel = _parallel,
                                             verbose = verbose)
 
-
     # defining indices per mode grouping -----------
     simulated_Z_idx_group_list = [
         np.arange(group_df.shape[0])[group_df.group == g_idx]
         for g_idx in np.arange(num_groups)]
+
+    # small-ball-radius value --------------
+    mode_radius_info_global = []
+    mode_radius_info_per_time = []
+
+    for m_idx in np.arange(num_groups):
+        sbr_mat = calc_small_ball_rad_multidim_func(simulated_Z_array[simulated_Z_idx_group_list[m_idx],:,:],
+                                                       pd_vec[simulated_Z_idx_group_list[m_idx]],
+                                                       per_time=True)
+        sbr_vec_per_time = sbr_mat[int(np.ceil(sbr_mat.shape[0]*_sbr_prop)-1),:]
+
+        mode_radius_info_per_time.append(sbr_vec_per_time)
+
+
+        sbr_start_vec = calc_small_ball_rad_multidim_func(simulated_Z_array[simulated_Z_idx_group_list[m_idx],:,:],
+                                                       pd_vec[simulated_Z_idx_group_list[m_idx]],
+                                                       per_time=False)
+
+        sbr_vec_global = sbr_start_vec[int(np.ceil(sbr_start_vec.shape[0]*_sbr_prop)-1)]
+
+        mode_radius_info_global.append(sbr_vec_global)
+
+
+
 
     # defining df with conformal score values (across modes) --------
     def inner_rank(df, name_string = "local_rank"):
@@ -139,25 +156,47 @@ def simulation_based_conformal(true_z,
         bar = progressbar.ProgressBar()
         mode_iter = bar(mode_iter)
 
-    true_cs_per_mode = pd.DataFrame()
+    true_cs_per_mode_per_time = pd.DataFrame()
+    true_cs_per_mode_global = pd.DataFrame()
 
     for mode_idx in mode_iter:
         sim_Z_sub = simulated_Z_array[simulated_Z_idx_group_list[mode_idx]]
         pd_vec_sub = pd_vec_group_list[mode_idx]
 
-        contained_idx = when_contained(true_z = true_z,
+        sbr_per_time = mode_radius_info_per_time[mode_idx]
+        sbr_global = mode_radius_info_global[mode_idx]
+
+        # per time -------------
+        contained_idx_per_time = when_contained(true_z = true_z,
                                        simulated_Z_array = sim_Z_sub,
                                        pd_vec = pd_vec_sub,
-                                       sbr = sbr_val,
-                                       per_time = _per_time)
-        inner_true_cs_df = pd.DataFrame(data = dict(group = [mode_idx],
-                                                    local_rank = 1.0*np.array([contained_idx])))
+                                       sbr = sbr_per_time,
+                                       per_time = True)
+        inner_true_cs_df_per_time = pd.DataFrame(data = dict(group = [mode_idx],
+                                                    local_rank = 1.0*np.array([contained_idx_per_time])))
 
-        true_cs_per_mode = pd.concat((true_cs_per_mode, inner_true_cs_df))
+        true_cs_per_mode_per_time = pd.concat((true_cs_per_mode_per_time, inner_true_cs_df_per_time))
 
-    individual_info = pd.merge(true_cs_per_mode, full_ranking_from_mode_list_df,
+        # global ---------------
+        contained_idx_global = when_contained(true_z = true_z,
+                                       simulated_Z_array = sim_Z_sub,
+                                       pd_vec = pd_vec_sub,
+                                       sbr = sbr_global,
+                                       per_time = False)
+        inner_true_cs_df_global = pd.DataFrame(data = dict(group = [mode_idx],
+                                                    local_rank = 1.0*np.array([contained_idx_global])))
+
+
+        true_cs_per_mode_global = pd.concat((true_cs_per_mode_global, inner_true_cs_df_global))
+
+    individual_info_per_time = pd.merge(inner_true_cs_df_per_time, full_ranking_from_mode_list_df,
              how = "left", on = ["group", "local_rank"])
 
-    cs_score = individual_info["global_rank"].min()
+    cs_score_per_time = B - individual_info_per_time["global_rank"].min()
 
-    return cs_score
+    individual_info_global = pd.merge(inner_true_cs_df_global, full_ranking_from_mode_list_df,
+         how = "left", on = ["group", "local_rank"])
+
+    cs_score_global = B - individual_info_global["global_rank"].min()
+
+    return cs_score_per_time, cs_score_global
--- package/orbconformal/tests/test_conformal_prediction.py
+++ package/orbconformal/tests/test_conformal_prediction.py
@@ -42,7 +42,7 @@ def test_simulation_based_conformal():
 
 
 
-    cs_z_50 = oc.simulation_based_conformal(true_z = z1,
+    cs_z_50_pt, cs_z_50_g = oc.simulation_based_conformal(true_z = z1,
                                simulated_z = z_multi2,
                                _sigma_string = "20%",
                                _small_ball_radius_string = "50%",
@@ -52,7 +52,7 @@ def test_simulation_based_conformal():
                                _parallel = 1,
                                verbose = False)
 
-    cs_z_20 = oc.simulation_based_conformal(true_z = z1,
+    cs_z_20_pt, cs_z_20_g = oc.simulation_based_conformal(true_z = z1,
                                simulated_z = z_multi2,
                                _sigma_string = "20%",
                                _small_ball_radius_string = "20%",
@@ -62,9 +62,10 @@ def test_simulation_based_conformal():
                                _parallel = 1,
                                verbose = False)
 
-    assert cs_z_50 > cs_z_20, \
-        "we expect on average that a large small-ball-radius percentage," +\
-        "so conformal score should be larger"
+    assert cs_z_50_g > cs_z_20_g, \
+        "in this case we expect on average that a large "+\
+        "small-ball-radius percentage," +\
+        "so conformal score should be larger (bad static test anyway...)"
 
     # extremely static
     almost_zero = np.zeros(z1.shape)
@@ -72,10 +73,10 @@ def test_simulation_based_conformal():
 
     static_almost_zero_change = np.concatenate([
         (z1 + i*almost_zero).reshape((1, z1.shape[0],z1.shape[1]))
-             for i in np.array([.5, 1, 2, 5])])
+             for i in np.array([.5, 1, 2, 5] + [.5*-1, 1*-1, 2*-1, 5*-1])])
 
 
-    cs_z_static = oc.simulation_based_conformal(true_z = z1,
+    cs_z_static_pt, cs_z_static_g  = oc.simulation_based_conformal(true_z = z1,
                                simulated_z = static_almost_zero_change,
                                _sigma_string = "50%",
                                _small_ball_radius_string = "70%",
@@ -85,16 +86,16 @@ def test_simulation_based_conformal():
                                _parallel = 1,
                                verbose = False)
 
-    assert cs_z_static < 4, \
+    assert cs_z_static_g > 0, \
         "observation should be contained in some of prediction "+\
         "region if not too far away"
 
     static_almost_zero_change2 = np.concatenate([
         (z1 + i*almost_zero).reshape((1, z1.shape[0],z1.shape[1]))
-             for i in np.array([.5,.75, 1, 2, 5])+10])
+             for i in np.array([.5, 1, 2, 5] + [.5*-1, 1*-1, 2*-1, 5*-1])+10])
 
 
-    cs_z_static3 = oc.simulation_based_conformal(true_z = z1,
+    cs_z_static3_pt, cs_z_static3_g = oc.simulation_based_conformal(true_z = z1,
                                simulated_z = static_almost_zero_change2,
                                _sigma_string = "20%",
                                _small_ball_radius_string = "70%",
@@ -104,7 +105,7 @@ def test_simulation_based_conformal():
                                _parallel = 1,
                                verbose = False)
 
-    assert cs_z_static3 == 5, \
+    assert cs_z_static3_g == 0, \
         "static observation shouldn't be covered by any level sets"
 
 def test_simulation_based_conformal2():
@@ -146,9 +147,8 @@ def test_simulation_based_conformal2():
 
 
 
-    cs_z_50_per_time = oc.simulation_based_conformal(true_z = z1,
+    cs_z_50_per_time, cs_z_50 = oc.simulation_based_conformal(true_z = z1,
                                simulated_z = z_multi2,
-                               _per_time=True,
                                _sigma_string = "20%",
                                _small_ball_radius_string = "50%",
                                _maxT = maxT,
@@ -157,20 +157,8 @@ def test_simulation_based_conformal2():
                                _parallel = 1,
                                verbose = False)
 
-    cs_z_50 = oc.simulation_based_conformal(true_z = z1,
+    cs_z_20_per_time, cs_z_20 = oc.simulation_based_conformal(true_z = z1,
                                simulated_z = z_multi2,
-                               _per_time=False,
-                               _sigma_string = "20%",
-                               _small_ball_radius_string = "50%",
-                               _maxT = maxT,
-                               _eps = eps,
-                               _diff_eps = diff_eps,
-                               _parallel = 1,
-                               verbose = False)
-
-    cs_z_20_per_time = oc.simulation_based_conformal(true_z = z1,
-                               simulated_z = z_multi2,
-                               _per_time=True,
                                _sigma_string = "20%",
                                _small_ball_radius_string = "20%",
                                _maxT = maxT,
@@ -180,16 +168,6 @@ def test_simulation_based_conformal2():
                                verbose = False)
 
 
-    cs_z_20 = oc.simulation_based_conformal(true_z = z1,
-                               simulated_z = z_multi2,
-                               _per_time=False,
-                               _sigma_string = "20%",
-                               _small_ball_radius_string = "20%",
-                               _maxT = maxT,
-                               _eps = eps,
-                               _diff_eps = diff_eps,
-                               _parallel = 1,
-                               verbose = False)
 
     assert cs_z_50_per_time == cs_z_50, \
         "given the addition of the same value across obs, should see same " +\
@@ -205,12 +183,11 @@ def test_simulation_based_conformal2():
 
     static_almost_zero_change = np.concatenate([
         (z1 + i*almost_zero).reshape((1, z1.shape[0],z1.shape[1]))
-             for i in np.array([.5, 1, 2, 5])])
+             for i in np.array([.5, 1, 2, 5] + [.5*-1, 1*-1, 2*-1, 5*-1])])
 
 
-    cs_z_static = oc.simulation_based_conformal(true_z = z1,
+    cs_z_static, _ = oc.simulation_based_conformal(true_z = z1,
                                simulated_z = static_almost_zero_change,
-                               _per_time=True,
                                _sigma_string = "50%",
                                _small_ball_radius_string = "70%",
                                _maxT = maxT,
@@ -219,18 +196,17 @@ def test_simulation_based_conformal2():
                                _parallel = 1,
                                verbose = False)
 
-    assert cs_z_static < 4, \
+    assert cs_z_static > 0, \
         "observation should be contained in some of prediction "+\
         "region if not too far away (per_time=T, only 1 value change)"
 
     static_almost_zero_change2 = np.concatenate([
         (z1 + i*almost_zero).reshape((1, z1.shape[0],z1.shape[1]))
-             for i in np.array([.5,.75, 1, 2, 5])+10])
+             for i in np.array([.5, 1, 2, 5] + [.5*-1, 1*-1, 2*-1, 5*-1])+10])
 
 
-    cs_z_static3 = oc.simulation_based_conformal(true_z = z1,
+    cs_z_static3, _ = oc.simulation_based_conformal(true_z = z1,
                                simulated_z = static_almost_zero_change2,
-                               _per_time=True,
                                _sigma_string = "20%",
                                _small_ball_radius_string = "70%",
                                _maxT = maxT,
@@ -239,70 +215,46 @@ def test_simulation_based_conformal2():
                                _parallel = 1,
                                verbose = False)
 
-    assert cs_z_static3 == 5, \
+    assert cs_z_static3 == 0, \
         "static observation shouldn't be covered by any level sets "+\
         "(per_time=T, only 1 value change)"
 
 
     # extremely static (across time...)
-    almost_zero_row = np.zeros(z1.shape)
-    almost_zero_row[:,3] = np.arange(z1.shape[0]) + 1
+    almost_zero_change_row = np.zeros((3,z1.shape[0],z1.shape[1]))
+    almost_zero_change_row[:,:5,3] = np.array([[.1,.45,.2,.1,0],
+                                             [.15,.4,.25,.15,.05],
+                                             [.5,.5,.5,.5,.5]])
 
-    static_almost_zero_change_row = np.concatenate([
-        (z1 + i*almost_zero_row).reshape((1, z1.shape[0],z1.shape[1]))
-             for i in np.array([.5, 1, 2, 5])])
 
-    cs_z_static_row70 = oc.simulation_based_conformal(true_z = z1,
-                               simulated_z = static_almost_zero_change_row,
-                               _per_time=False,
-                               _sigma_string = "40%",
-                               _small_ball_radius_string = "70%",
-                               _maxT = maxT,
-                               _eps = eps,
-                               _diff_eps = diff_eps,
-                               _parallel = 1,
-                               verbose = False)
-    cs_z_static_row_per_time70 = oc.simulation_based_conformal(true_z = z1,
-                           simulated_z = static_almost_zero_change_row,
-                           _per_time=True,
-                           _sigma_string = "40%",
-                           _small_ball_radius_string = "70%",
-                           _maxT = maxT,
-                           _eps = eps,
-                           _diff_eps = diff_eps,
-                           _parallel = 1,
-                           verbose = False)
+    static_almost_zero_change_row = almost_zero_change_row
+
+
+
+    z_new = np.zeros(z1.shape)
 
     out = list()
-    for sbr_string in [str(x)+"%" for x in np.arange(1,10,dtype = int)*10]:
-        cs_z_static_row = oc.simulation_based_conformal(true_z = z1,
+    for sbr_string in [str(x)+"%" for x in [10,30, 90]]:
+        cs_z_static_row_per_time, cs_z_static_row = oc.simulation_based_conformal(true_z = z_new,
                                    simulated_z = static_almost_zero_change_row,
-                                   _per_time=False,
-                                   _sigma_string = "40%",
+                                   _sigma_string = "80%",
                                    _small_ball_radius_string = sbr_string,
                                    _maxT = maxT,
                                    _eps = eps,
                                    _diff_eps = diff_eps,
                                    _parallel = 1,
                                    verbose = False)
-        cs_z_static_row_per_time = oc.simulation_based_conformal(true_z = z1,
-                               simulated_z = static_almost_zero_change_row,
-                               _per_time=True,
-                               _sigma_string = "40%",
-                               _small_ball_radius_string = sbr_string,
-                               _maxT = maxT,
-                               _eps = eps,
-                               _diff_eps = diff_eps,
-                               _parallel = 1,
-                               verbose = False)
 
         out.append([cs_z_static_row,cs_z_static_row_per_time])
 
     out_np = np.array(out)
     assert np.any(out_np[:,0] != out_np[:,1]), \
         "we expect at least of few times (relative to _sbr_string) "+\
-        "on _sigma_str = 40% where _per_time impacts the containment "+\
+        "on _sigma_str = 50% where _per_time impacts the containment "+\
         "(static example)"
+
+    assert out_np[2,0] == 3 and out_np[2,1] == 0, \
+        "expect global region to contain point but not per_time approach"
 
 
 
